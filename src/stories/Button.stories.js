688df975d20715583f564b7cb03a9a9a897e3b8d
VERTICA\mih <mih@vertica.dk>
2022-03-20 14:00:51 +0100

    Adding new code

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -9,11 +9,27 @@ export default {
     onClick: {},
   },
   parameters: {
-    design: {
-       type: 'figma',
-       url: 'https://www.figma.com/file/HSFEnRMy14U46klJNRPVPF/vuetify-ui-dark-kit?node-id=0%3A1',
-       allowFullscreen: true
-    }
+    design: [
+      //Notes to self - the link in figma needs to be wrapped in a "Frame" like the test knap is wrapped in a frame element in figma
+      {
+        name: 'Random Button Design',
+        type: 'figspec',
+        url: 'https://www.figma.com/file/kAWGJlcI0mTGbDHA1TkW3c/testknap?node-id=23%3A4',
+        accessToken: '344633-7d670d90-0b50-432c-a9a9-24af2c5edade',
+      },
+      {
+        name: 'Hørkram Design - Regional Items',
+        type: 'figspec',
+        url: 'https://www.figma.com/file/x0qTcVhs4GGYVVqiNsNK1w/Regional-items?node-id=11%3A601',
+        accessToken: '344633-7d670d90-0b50-432c-a9a9-24af2c5edade',
+      },
+      {
+        name: 'Vuetify UI Kit',
+        type: 'figspec',
+        url: 'https://www.figma.com/file/rRZ7nGMHU1iIttGZqhOQ7R/vuetifytest?node-id=102%3A2',
+         accessToken: '344633-7d670d90-0b50-432c-a9a9-24af2c5edade',
+      },
+    ],
  }
 };
 
@@ -36,7 +52,7 @@ Primary.args = {
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'Button secondary',
 };
 
 export const Large = Template.bind({});
